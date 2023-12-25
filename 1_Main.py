298d88b596eed9a9910934f4c2ad379e13d1245c
fkillinger <fritz.killinger@gmail.com>
2023-12-25 18:12:06 +0100

    Test

---
+++ 1_Main.py
@@ -0,0 +1,84 @@
+# Webpage mit python und streamlit
+from PIL import Image
+import streamlit as st
+import requests
+from streamlit_lottie import st_lottie
+
+st.set_page_config(page_title="KC Webspace", page_icon=":sun:", layout="wide")
+st.markdown("""
+    <style>
+    .font {
+        font-size:18px !important;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+# lottie_coding = load_lottieurl("https://lottie.host/2d8650da-67dd-4f88-b3da-27da9474e710/KVib0lIpf3.json") # background white
+lottie_coding = load_lottieurl("https://lottie.host/33e14c93-4f6e-446d-8417-ed82b79e82fe/ykkTfkPb9S.json")   # bg black
+
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style/style.css")
+
+img_kc_form = Image.open("images/kc_Feder_logo.jpg")
+
+#st.title("Main Page")
+st.sidebar.success("Seitenwahl")
+
+# ---- Header section -----
+with st.container():
+    #st.subheader("Meine neue Webseite")
+    left_column, right_column = st.columns(2)
+
+    with left_column:
+        st.write("#")
+        st.title("Killinger Consulting")
+
+    with right_column:
+        st_lottie(lottie_coding, height=160, key="coding")
+
+# --- what I am doing ---
+with st.container():
+    left_column, image_column, right_column = st.columns([2,1,2])
+    with left_column:
+        st.subheader("Unser Beratungsangebot:")
+        st.markdown(        #'<p class="big-font">Hello World !!</p>', unsafe_allow_html=True
+            """<p class="font">      
+             &nbsp; - Messtechnik <br>           
+             &nbsp; - Prüftechnik <br>          
+             &nbsp; - Datenbanken <br>
+             &nbsp; - Mikro - Controller Anwendungen <br>
+             &nbsp; - Programmierung <br>
+            
+        Wenn es interessant klingt,
+        dann gehen Sie auf das
+        Kontakt - Formular.   
+            """, unsafe_allow_html=True)
+
+    with image_column:
+        st.write("##")
+        st.image(img_kc_form)
+
+    with right_column:
+        st.subheader("Unsere Dienstleistungen:")
+        st.markdown(
+            """<p class="font">  
+              &nbsp;  - Dokumentationen <br>   
+              &nbsp;  - Erstellung von Expertisen <br>  
+              &nbsp;  - Programmierung mit Python  <br> 
+              &nbsp;  - Vorentwicklungen <br>   
+              &nbsp;  - Leiterplattenentwicklung und Entflechtung <br>                     
+              &nbsp;  - CAD für 3D - Druck Modelle <br>   
+              &nbsp;  - Gutachten <br>   
+                """, unsafe_allow_html=True)
+
